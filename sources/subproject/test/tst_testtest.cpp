078cb442a3b88704fbfd5f92a67a9fa2e74589ea
BulgakovaMargarita <bulgakova1997@mail.ru>
2015-12-24 17:21:13 +0300

    Добавлены дополнительные тесты

--- sources/subproject/test/tst_testtest.cpp
+++ sources/subproject/test/tst_testtest.cpp
@@ -30,15 +30,14 @@ void TestTest::testCase1()
 }
 
 
-/// Надо добавить еще тестовых вариантов
 void TestTest::test_max_composite_number_function(){
     QCOMPARE(findings_max_composite_number(1038,5147),5148);
+    QCOMPARE(findings_max_composite_number(38,500),538);
 }
 
-/// Вы проверяете здесь только один вариант угрозы, было бы хорошо проверить все или почти все,
-/// можно дописать в этот же тест
 void TestTest::test_treat_to_king_of_chess_function(){
     QCOMPARE(treats_to_king_of_chess(1,3,2,2,2,1,4,5),2);
+    QCOMPARE(treats_to_king_of_chess(3,5,2,2,2,1,4,5),3);
 }
 
 void TestTest::test_i_coefficient_of_equation_function(){
